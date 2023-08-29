600c07552047fbc488a4356b1896bc56e980b81f
AubBantolo <jhuliet14@gmail.com>
2023-08-29 21:14:03 +0800

    feat: added reducers

--- src/components/todoListSlice.js
+++ src/components/todoListSlice.js
@@ -4,19 +4,24 @@ const todoListSlice = createSlice({
     name: "todoList",
     initialState: {
         todoList: [],
-    },
+    }, 
     reducers: {
         addTodoItem: (state, action) => {
             state.todoList.push(action.payload);
-			//console.log(state.todoList.push(action.payload));
         },
+        
         doneTodoItem: (state, action) => {
-            //update
-            
-            console.log(action);
+            const todoItem = state.todoList.find(item => item.id !== action.payload.id);
+            if(todoItem) {
+                todoItem.done = !todoItem.done;
+            }
         },
-    },
+
+        deleteTodo: (state, action) => {
+            state.todoList = state.todoList.filter(item => item.id !== action.payload);
+        },
+    }
 });
 
-export const { addTodoItem, doneTodoItem } = todoListSlice.actions;
+export const { addTodoItem, doneTodoItem, deleteTodo} = todoListSlice.actions;
 export default todoListSlice.reducer;

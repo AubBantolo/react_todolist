9c46c24c45661e8cf8147e34e331c4595a5cba34
AubBantolo <jhuliet14@gmail.com>
2023-08-30 18:42:06 +0800

    feat: applied ant d

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
 import './App.css';
 import { Outlet, NavLink } from 'react-router-dom';
+import { HomeOutlined, CheckCircleOutlined, QuestionCircleOutlined } from '@ant-design/icons';
 
 function App() {
   return (
     <div className="App">
       <div className='navBar'>
         <nav>
-          <ul>
-            <li><NavLink to={'/'}>Home</NavLink></li>
-            <li><NavLink to={'/done'}>Done List</NavLink></li>
-            <li><NavLink to={'/help'}>Help</NavLink></li>
+          <ul className="nav-list-item">
+            <li>
+              <NavLink to={'/'}>
+                <HomeOutlined /> Home
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to={'/done'}>
+                <CheckCircleOutlined /> Done List
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to={'/help'}>
+                <QuestionCircleOutlined /> Help
+              </NavLink>
+            </li>
           </ul>
         </nav>
       </div>
@@ -17,8 +30,11 @@ function App() {
       <h1>📋 Todo List</h1>
       <Outlet></Outlet>
       
-  </div>
+    </div>
   );
 }
 
 export default App;
+
+
+

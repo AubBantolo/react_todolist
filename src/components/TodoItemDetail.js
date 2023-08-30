dc69f2c42b2c3501b3852c2f79f81a58b023a5ed
AubBantolo <jhuliet14@gmail.com>
2023-08-30 20:49:10 +0800

    feat: added css

---
+++ src/components/TodoItemDetail.js
@@ -0,0 +1,16 @@
+import { useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
+
+export const TodoItemDetail = () => {
+    const { id } = useParams();
+    const todoItem = useSelector((state) => state.todoList.todoList.find((todoItem) => todoItem.id === id));
+    return (
+        <div className="todo-detail">
+            <h3>Todo Item Detail</h3>
+            <div className="todo-content">
+                <div id="id">{todoItem.id}</div>
+                <div id="text">{todoItem.text}</div>
+            </div>
+        </div>
+    );
+}

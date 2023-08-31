c43da03b06e89cff7db41a62074c22c2dccbb9f4
AubBantolo <jhuliet14@gmail.com>
2023-08-31 15:42:29 +0800

    feat: changed base url

--- src/api/api.js
+++ src/api/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const api = axios.create({
-    baseURL: "https://64edbef81f8721827141ae4d.mockapi.io/",
+    baseURL: "http://localhost:8080/",
 });
 
 export default api;
